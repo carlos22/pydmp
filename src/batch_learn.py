corrected calculation of yd ydd - now working

src/batch_learn.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python
 '''
-Simple DMP example (plotting)
+DMP Adaption
 '''
 import pylab as plt
 import numpy as np
-
-from dmp import DiscreteDMP
-from plot_tools import plot_pos_vel_acc_trajectory
-
 import json
+from dmp import DiscreteDMP
 
 def main():
   
   # duration
   duration = 1.0
   
+  # adaption offset
+  adapt_offset = -0.02
+  
   # time steps
-  delta_t = 0.01
+  delta_t = 0.001
   
-  # trajectory is a list of 3-tuples with (pos,vel,acc)
+  # load position trajectory
+  traj_pos = json.load(open("traj_full.json", 'r'))["x"][2000:8000][::6]
   
-  # load trajectory
-  traj_pos = json.load(open("traj_full.json", 'r'))["x"][0:12000][::120]
-  
-  # move trajectory to start at 0
-  #traj_pos = np.asarray(traj_pos) - traj_pos[0]
-  
+  # rest start and goal position out of trajectory
   start = traj_pos[0]
   goal = traj_pos[-1]
-  
-  traj_vel = list(np.diff(traj_pos))
-  traj_vel.insert(0, 0.0)
-  traj_acc = list(np.diff(traj_vel))
-  traj_acc.insert(0, 0.0)
-  
-  traj = np.asarray(zip(traj_pos, traj_vel, traj_acc))
     
   traj_freq = int(1.0 / delta_t)
+  
+  # calc it here for easier drawing
+  traj = DiscreteDMP.compute_derivatives(traj_pos, traj_freq)
 
   ####### learn DMP
-  
+   
   dmp = DiscreteDMP()
+  dmp.use_ft = True
   dmp.learn_batch(traj, traj_freq)
+  
+  
+  dmp_adapt = DiscreteDMP()
+  dmp_adapt.use_ft = True
+  dmp_adapt.learn_batch(traj, traj_freq)
   
   
   ####### learn DMP
@@ -50,8 +48,13 @@ def main():
   dmp.delta_t = delta_t
   dmp.setup(start, goal, duration)
   
+  dmp_adapt.delta_t = delta_t  
+  dmp_adapt.setup(start, goal + adapt_offset, duration)
+  
+  
   # trajectory
-  reproduced_traj = []
+  traj_reproduced = []
+  traj_adapted = []
   
   # states of canonical system (plotting)
   s = []
@@ -65,68 +68,82 @@ def main():
     
     # run a single integration step
     dmp.run_step()
+    dmp_adapt.run_step()
     
     # remember canonical system values
     s.append(dmp.s)
     s_time.append(dmp.s_time)
     
     # save reproduced trajectory
-    reproduced_traj.append([dmp.x, dmp.xd, dmp.xdd])
+    traj_reproduced.append([dmp.x, dmp.xd, dmp.xdd])
+    traj_adapted.append([dmp_adapt.x, dmp_adapt.xd, dmp_adapt.xdd])
+  
 
 
   ####### PLOTTING
 
   
   # create figure
-  fig = plt.figure('dmp batch learn from min jerk', figsize=(14, 4.6))
+  fig = plt.figure('dmp', figsize=(6, 4))
   # create axes
-  ax_pos = fig.add_subplot(131)
-  ax_vel = fig.add_subplot(132)
-  ax_acc = fig.add_subplot(133)
+  ax_pos = fig.add_subplot(111)
+    
     
   # plot on the axes
   #ax_pos.plot(np.arange(0,1,0.001), traj_pos)
-  plot_pos_vel_acc_trajectory((ax_pos, ax_vel, ax_acc), traj, delta_t, label='demonstration')
-  plot_pos_vel_acc_trajectory((ax_pos, ax_vel, ax_acc), reproduced_traj, dmp.delta_t, label='reproduction', linestyle='dashed')
+  plot_time = np.arange(len(traj)) * delta_t
+  
+  ax_pos.plot(plot_time, np.asarray(traj)[:, 0], label='demonstration')
+  ax_pos.plot(plot_time, np.asarray(traj_reproduced)[:, 0], label='reproduction', linestyle='dashed')
+  ax_pos.plot(plot_time, np.asarray(traj_adapted)[:, 0], label='adapted (%+0.2f)' % adapt_offset)
+  
+  ax_pos.legend(loc='lower left')
+  
+  
+#  plot_pos_vel_acc_trajectory((ax_pos, ax_vel, ax_acc), traj, dmp.delta_t, label='demonstration')
+#  plot_pos_vel_acc_trajectory((ax_pos, ax_vel, ax_acc), traj_reproduced, dmp.delta_t, label='reproduction', linestyle='dashed')
+#  plot_pos_vel_acc_trajectory((ax_pos, ax_vel, ax_acc), traj_adapted, dmp.delta_t, label='adapted (+0.02)')
   
   fig.tight_layout()
   
   plt.show()
   
-  fig = plt.figure('ftarget', figsize=(14, 4.6))
   
-  # plot  ftarget (real and predicted)
-  ax_ft = fig.add_subplot(131)
-  ax_ft.plot(dmp.target_function_input, dmp.target_function_ouput, linewidth=2, label=r'$f_{target}(s)$')
-  ax_ft.plot(dmp.target_function_input, dmp.target_function_predicted, '--', linewidth=2, label=r'$f_{predicted}(s)$') #dashes=(3, 3)
-  ax_ft.set_xlabel(r'$s$')
-  ax_ft.set_ylabel(r'$f(s)$')
-  ax_ft.legend()
   
-  # kernels (lwr)
-  ax_kernel = fig.add_subplot(132)
-  dmp.lwr_model.plot_kernels(ax_kernel)
-  ax_kernel.set_xlabel(r'$s$')
-  ax_kernel.set_ylabel(r'$\psi(s)$')
-  
-  # canonical system
-#  ax_cs = fig.add_subplot(236)
-#  ax_cs.plot(s_time, s)
-#  ax_cs.set_xlabel('Time [s]')
-#  ax_cs.set_ylabel(r'$s$')
-
-
-  # weights of kernels (w)
-  ax_w = fig.add_subplot(133)
-  lwr_wights = dmp.lwr_model.get_thetas()
-  ax_w.bar(range(len(lwr_wights)), lwr_wights, width=0.3)
-  ax_w.set_xlabel('$w_i$')
-  ax_w.set_ylabel('')
-
-
-  #fig.tight_layout()
-  
-  plt.show()  
+#  fig = plt.figure('ftarget', figsize=(14, 4.6))
+#  
+#  # plot  ftarget (real and predicted)
+#  ax_ft = fig.add_subplot(131)
+#  ax_ft.plot(dmp.target_function_input, dmp.target_function_ouput, linewidth=2, label=r'$f_{target}(s)$')
+#  ax_ft.plot(dmp.target_function_input, dmp.target_function_predicted, '--', linewidth=2, label=r'$f_{predicted}(s)$') #dashes=(3, 3)
+#  ax_ft.set_xlabel(r'$s$')
+#  ax_ft.set_ylabel(r'$f(s)$')
+#  ax_ft.legend()
+#  
+#  # kernels (lwr)
+#  ax_kernel = fig.add_subplot(132)
+#  dmp.lwr_model.plot_kernels(ax_kernel)
+#  ax_kernel.set_xlabel(r'$s$')
+#  ax_kernel.set_ylabel(r'$\psi(s)$')
+#  
+#  # canonical system
+##  ax_cs = fig.add_subplot(236)
+##  ax_cs.plot(s_time, s)
+##  ax_cs.set_xlabel('Time [s]')
+##  ax_cs.set_ylabel(r'$s$')
+#
+#
+#  # weights of kernels (w)
+#  ax_w = fig.add_subplot(133)
+#  lwr_wights = dmp.lwr_model.get_thetas()
+#  ax_w.bar(range(len(lwr_wights)), lwr_wights, width=0.3)
+#  ax_w.set_xlabel('$w_i$')
+#  ax_w.set_ylabel('')
+#
+#
+#  fig.tight_layout()
+#  
+#  plt.show()  
   
 
 if __name__ == '__main__':

src/dmp.py
@@ -63,6 +63,9 @@ class DiscreteDMP:
     # debugging (y values predicted by fitted lwr model)
     self.target_function_predicted = None
 
+    # do not predict f by approximated function, use values of ft directly - only works if duration is 1.0!!
+    self.use_ft = False
+
     # create LWR model and set parameters
     self.lwr_model = LWR(activation=0.1, exponentially_spaced=True, n_rfs=20)
     
@@ -173,6 +176,42 @@ class DiscreteDMP:
     
     return target_function_input, np.asarray(target_function_ouput)
   
+  @staticmethod
+  def compute_derivatives(pos_trajectory, frequency):
+    # ported from trajectory.cpp 
+    # no fucking idea why doing it this way - but hey, the results are better ^^
+    
+    add_pos_points = 4
+    #add points to start
+    for _ in range(add_pos_points):
+      first_point = pos_trajectory[0]
+      pos_trajectory.insert(0, first_point)
+      
+    # add points to the end
+    for _ in range(add_pos_points):
+      first_point = pos_trajectory[-1]
+      pos_trajectory.append(first_point)
+      
+    # derive positions
+    vel_trajectory = []
+    
+    for i  in range(len(pos_trajectory) - 4):
+      vel = (pos_trajectory[i] - (8.0 * pos_trajectory[i + 1]) + (8.0 * pos_trajectory[i + 3]) - pos_trajectory[i + 4]) / 12.0
+      vel *= frequency
+      vel_trajectory.append(vel)
+    
+    
+    # derive velocities
+    acc_trajectory = []
+    for i  in range(len(vel_trajectory) - 4):     
+      acc = (vel_trajectory[i] - (8.0 * vel_trajectory[i + 1]) + (8.0 * vel_trajectory[i + 3]) - vel_trajectory[i + 4]) / 12.0
+      acc *= frequency
+      acc_trajectory.append(acc)
+        
+    result_traj = zip(pos_trajectory[4:], vel_trajectory[2:], acc_trajectory)
+  
+    return result_traj
+  
   def learn_batch(self, sample_trajectory, frequency):
     '''
      Learns the DMP by a given sample trajectory
@@ -180,9 +219,13 @@ class DiscreteDMP:
     '''
     assert len(sample_trajectory) > 0
     
+    if isinstance(sample_trajectory[0], float):
+      # calculate yd and ydd if sample_trajectory does not contain it
+      sample_trajectory = self.compute_derivatives(sample_trajectory, frequency)
+      #raise NotImplementedError("automatic derivation of yd and ydd not yet implemented!")
+    
     if len(sample_trajectory[0]) != 3:
-      # TODO: calculate yd and ydd if sample_trajectory does not contain it
-      raise NotImplementedError("automatic derivation of yd and ydd not yet implemented!")
+      raise Exception("malformed trajectory, has to be a list with 3-tuples [(1,2,3),(4,5,6)]")
     
     # get input and output of desired target function
     target_function_input, target_function_ouput = self._create_training_set(sample_trajectory, frequency)
@@ -222,14 +265,15 @@ class DiscreteDMP:
     
     # update f(s)
     # debugging: use raw function output (time must be 1.0)
-    ftinp = list(self.target_function_input)
-    ft = self.target_function_ouput[ftinp.index(self.s)]
-    self.f = ft
+    if self.use_ft:
+      print "DEBUG: using ft without approximation"
+      ftinp = list(self.target_function_input)
+      ft = self.target_function_ouput[ftinp.index(self.s)]
+      self.f = ft
+    else:
+      f = self.predict_f(self.s)
+      self.f = f
     
-    #f = self.predict_f(self.s)
-    #self.f = f
-    
-    #print f - ft
     
     # calculate xdd (vd) according to the transformation system equation 1
     self.xdd = (self.k_gain * (self.goal - self.x) - self.d_gain * self._raw_xd + (self.goal - self.start) * self.f) / self.tau